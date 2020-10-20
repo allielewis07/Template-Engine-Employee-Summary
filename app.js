fd410a6491466bb0e77e916f17a0e63ae250d01f
allie Lewis <allielewis07@gmail.com>
2020-10-19 22:53:46 -0400

    modiefied and tested

--- app.js
+++ app.js
@@ -18,65 +18,127 @@ function questions() {
         name: "name"
     },
     {
-        type: "list",
-        message: "What is the role of the team member you would like to add?",
-        choices: [
-            "Manager",
-            "Intern",
-            "Engineer"
-        ],
-        name: "role"
-    },
-    {
         message: "Enter team member's Id",
         name: "id"
     },
     {
         message: "Enter team member's email address",
         name: "email"
+    },
+    { message: "Enter Your office Number:",
+    name: "officeNumber"
+    } ,
+    {
+        type: "list",
+        name: "membersChoice",
+        message: "Would you like to add another team member? If so which type?",
+        choices: [
+            "Intern",
+            "Engineer",
+            "No, Thank you."
+        ],
     }])
         // different credentials per role
-        .then(function ({ name, role, id, email }) {
+        .then(function (answer) {
             let roleInfo = "";
-            if (roleInfo === Engineer) {
-                roleInfo = "github username";
+            if (answer.membersChoice === "Engineer") {
+                addEngineer();
             }
-            else if (roleInfo === Intern) {
-                roleInfo = "school name"
+            else if (answer.membersChoice === "Intern"){ 
+                addIntern();
+            } else if (answer.memberChoice === "No, Thank you."){
+                return; 
             }
-            else {
-                roleInfo = "office phone number"
-            }
-            inquirer.prompt([{
-                message: "What is your ${data.roleInfo}",
-                name: "roleInfo"
+        })
+    }
+    function addEngineer (){
+        inquirer
+        .prompt([
+        {
+            type:"input",
+            message: "What is your github?",
+            name: "github"
             },
             {
-                type: "confirm",
-                message: "Would you like to add another team member?",
-                name: "moreMembers",
-            }])
-                // function to add more team members
-                .then(function ({ roleInfo, moreMembers }) {
-                    let newMember;
-                    if (role === "Engineer") {
-                        newMember = new Engineer(name, id, email, roleInfo);
-                        employees.push (newMember)
-                    } else if (role === "Intern") {
-                        newMember = new Intern(name, id, email, roleInfo);
-                        employees.push (newMember)
-                    } else if (role === "Manager") {
-                        newMember = new Manager(name, id, email, roleInfo)
-                        employees.push (newMember)
-                    };
-                    // if yes: repeat file, if no: render to html
-                    if (moreMembers) {
-                        questions () 
-                    } else {
-                        var fs = require('fs')
-                        fs.writeFileSync (outputPath, render (employees), "utf8")
-                    }
-                })
+            type:"input",
+            message:"what is your name?",
+            name:"name"
+            },
+            {
+            type:"input",
+            message:"Enter team member's email address",
+            name:"email" 
+            },
+            { 
+            type:"input",
+            message:"What is your Id number?",
+            name: "id"
+            }
+            ])
+        .then(function(response){
+            const newEngineer = new Engineer(response.name,response.id,response.email,response.github)
+            teamMembers.push(newEngineer);
+            fs.writeFileSync(outputPath,render(teamMembers),'utf-8')
+        console.log("Team member has been created")
+            questions();
+            })
+        }
+    function addIntern(){
+        inquirer.prompt([
+            {
+                type:"input",
+                message:"what is your name?",
+                name:"name"
+                },
+            { 
+                type: "input",
+                message: "What is your id?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is your email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What school are you attending?",
+                name: "school"
+            }
+        ])
+        .then(function(response){
+            var newIntern = new Intern(response.name, response.id, response.email, response.school);
+            teamMembers.push(newIntern);
+            fs.writeFileSync(outputPath,render(teamMembers),'utf-8')
+            console.log("Team member has been created")
+            questions();
         })
-}
+    }
+//             inquirer.prompt([{
+//                 message:`What is your ${roleInfo}`,
+//                 name: "membersChoice"
+//             }])
+//                 // function to add more team members
+//                 .then(function ({ moreMembers }) {
+//                     let newMember;
+//                     if (role === "Engineer") {
+//                         newMember = new Engineer(name, id, email, github);
+//                         employees.push (newMember)
+//                     } else if (role === "Intern") {
+//                         newMember = new Intern(name, id, email, roleInfo);
+//                         employees.push (newMember)
+//                     } else if (role === "Manager") {
+//                         newMember = new Manager(name, id, email, officeNumber)
+//                         employees.push (newMember)
+//                     };
+//                     // if yes: repeat file, if no: render to html
+//                     if (moreMembers) {
+//                         questions () 
+//                     } else {
+//                         var fs = require('fs')
+//                         fs.writeFileSync (outputPath, render (employees), "utf8")
+//                     }
+//                 })
+//         })
+// }
 questions ();
